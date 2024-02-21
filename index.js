42da56f5b6fac36192e4dc2a3e574919f2eaa86f
jukaa57 <junioroliveira.1150@gmail.com>
2024-02-20 23:33:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { Server } from 'socket.io';
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('chat message', (msg, n) => {
+        io.emit('chat message', msg, n)
+    })
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+    })
+});
+
+server.listen(3033, () => console.log('server running at http://localhost:3033'));
